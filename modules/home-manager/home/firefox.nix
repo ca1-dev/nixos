285223624f74ad33b62708d130d750b4d8207d3d
ca1 <ca1@ca1.dev>
2025-05-09 19:00:49 +0100

    update firefox config

--- modules/home-manager/home/firefox.nix
+++ modules/home-manager/home/firefox.nix
@@ -8,53 +8,60 @@
         isDefault = true;
 
         search.force = true;
-        search.default = "DuckDuckGo";
+        search.default = "ddg";
         search.engines = {
-          "Bing".metaData.hidden = true;
-          "Google".metaData.hidden = true;
-          "Wikipedia (en)".metaData.hidden = true;
+          "bing".metaData.hidden = true;
+          "google".metaData.hidden = true;
+          "wikipedia".metaData.hidden = true;
         };
 
-        bookmarks = [{
-          name = "toolbar";
-          toolbar = true;
-          bookmarks = [
-            {
-              name = "";
-              url = "https://youtube.com/";
-            }
-            {
-              name = "";
-              url = "https://twitch.tv/";
-            }
-            {
-              name = "";
-              url = "https://tumblr.com/dashboard/";
-            }
-            {
-              name = "";
-              url = "https://github.com/";
-            }
-            {
-              name = "";
-              url = "https://monkeytype.com/";
-            }
-            {
-              name = "";
-              url = "https://orteil.dashnet.org/cookieclicker/";
-            }
-            {
-              name = "";
-              url = "https://wikipedia.org/";
-            }
-            {
-              name = "";
-              url = "https://developer.mozilla.org/";
-            }
-          ];
-        }];
+        bookmarks = {
+          force = true;
+          settings = [{
+            name = "toolbar";
+            toolbar = true;
+            bookmarks = [
+              {
+                name = "";
+                url = "https://youtube.com/";
+              }
+              {
+                name = "";
+                url = "https://twitch.tv/";
+              }
+              {
+                name = "";
+                url = "https://tumblr.com/dashboard/";
+              }
+              {
+                name = "";
+                url = "https://github.com/";
+              }
+              {
+                name = "";
+                url = "https://monkeytype.com/";
+              }
+              {
+                name = "";
+                url = "https://orteil.dashnet.org/cookieclicker/";
+              }
+              {
+                name = "";
+                url = "https://wikipedia.org/";
+              }
+              {
+                name = "";
+                url = "https://developer.mozilla.org/";
+              }
+              {
+                name = "";
+                url = "https://listen.tidal.com/";
+              }
+            ];
+          }];
+        };
 
-        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
           ublock-origin
           darkreader
           tampermonkey
@@ -75,11 +82,22 @@
           "browser.formfill.enable" = false; # disable all autofill
 
           # layout
+          "browser.toolbars.bookmarks.visibility" = "never";
+          "browser.newtabpage.activity-stream.feeds.system.topstories" = false;
           "browser.newtabpage.activity-stream.topSitesRows" = 0;
           "browser.newtabpage.activity-stream.logowordmark.alwaysVisible" = false;
           "browser.download.autohideButton" = true;
           "devtools.toolbox.host" = "right";
-          "browser.uiCustomization.state" = ''{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":[],"nav-bar":["back-button","forward-button","stop-reload-button","history-panelmenu","panic-button","urlbar-container","save-to-pocket-button","downloads-button","fxa-toolbar-menu-button","ublock0_raymondhill_net-browser-action","addon_darkreader_org-browser-action","firefox_tampermonkey_net-browser-action","unified-extensions-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["personal-bookmarks"]},"seen":["developer-button","ublock0_raymondhill_net-browser-action","addon_darkreader_org-browser-action","firefox_tampermonkey_net-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","unified-extensions-area","toolbar-menubar","TabsToolbar"],"currentVersion":20,"newElementCount":7}'';
+          "browser.tabs.inTitlebar" = 0; # get rid of close, maximise and minimise buttons in titlebar
+          "browser.uiCustomization.state" = ''{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":["addon_darkreader_org-browser-action","ublock0_raymondhill_net-browser-action","firefox_tampermonkey_net-browser-action","frankerfacez_frankerfacez_com-browser-action","adguard-extra-beta_adguard_com-browser-action"],"nav-bar":["back-button","forward-button","stop-reload-button","history-panelmenu","customizableui-special-spring16","vertical-spacer","urlbar-container","save-to-pocket-button","customizableui-special-spring9","downloads-button","fxa-toolbar-menu-button","unified-extensions-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":[],"vertical-tabs":["tabbrowser-tabs"],"PersonalToolbar":["personal-bookmarks"]},"seen":["developer-button","ublock0_raymondhill_net-browser-action","addon_darkreader_org-browser-action","firefox_tampermonkey_net-browser-action","frankerfacez_frankerfacez_com-browser-action","adguard-extra-beta_adguard_com-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","unified-extensions-area","toolbar-menubar","TabsToolbar","vertical-tabs"],"currentVersion":22,"newElementCount":16}'';
+          "browser.uiCustomization.navBarWhenVerticalTabs" = ''["back-button","forward-button","stop-reload-button","history-panelmenu","vertical-spacer","panic-button","urlbar-container","save-to-pocket-button","downloads-button","fxa-toolbar-menu-button","unified-extensions-button"]'';
+
+          # vertical tabs stuff
+          "sidebar.verticalTabs" = true;
+          "sidebar.new-sidebar.has-used" = true; # stop firefox from putting annoying stuff in the sidebar
+          "sidebar.revamp.round-content-area" = true;
+          "browser.toolbarbuttons.introduced.sidebar-button" = true; # stop toggle sidebar button from reappearing after restart
+          "sidebar.backupState" = ''{"panelOpen":false,"launcherWidth":250,"launcherExpanded":false,"launcherVisible":true}''; # make sidebar start minimised
 
           # disable useless features and popups
           "extensions.pocket.enabled" = false;
@@ -105,7 +123,7 @@
 
           # network stuff
           "security.OCSP.require" = true;
-          "network.trr.mode" = 3; # dns over https
+          "network.trr.mode" = 2; # dns over https if available
           "dom.security.https_only_mode" = true;
 
 
@@ -122,17 +140,16 @@
         id = 1;
 
         search.force = true;
-        search.default = "DuckDuckGo";
+        search.default = "ddg";
         search.engines = {
-          "Bing".metaData.hidden = true;
-          "Google".metaData.hidden = true;
-          "Wikipedia (en)".metaData.hidden = true;
+          "bing".metaData.hidden = true;
+          "google".metaData.hidden = true;
+          "wikipedia".metaData.hidden = true;
         };
 
-        extensions = with pkgs.nur.repos.rycee.firefox-addons;
-          [
-            ublock-origin
-          ];
+        extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
+          ublock-origin
+        ];
 
         settings = {
           "privacy.resistFingerprinting" = true;
@@ -153,6 +170,7 @@
           "browser.formfill.enable" = false; # disable all autofill
 
           # layout
+          "browser.newtabpage.activity-stream.feeds.system.topstories" = false;
           "browser.newtabpage.activity-stream.topSitesRows" = 0;
           "browser.newtabpage.activity-stream.logowordmark.alwaysVisible" = false;
           "browser.download.autohideButton" = true;
