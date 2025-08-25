852142a373c4cd8bb78e851ea57d98e39cc5c4c4
ca1 <ca1@ca1.dev>
2025-08-25 01:23:38 +0100

    refactor home manager config

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,9 +1,35 @@
 { home-manager, ... }: {
-  imports = [ ./options.nix ];
-  home-manager.users.ca1 = {
-    imports = [
-      ./themes/tokyonight
-      ./home
-    ];
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    backupFileExtension = "backup";
+
+    users.ca1 = {
+      imports = [
+        ./themes/tokyonight
+        ./firefox.nix
+      ];
+
+      home.pointerCursor = {
+        gtk.enable = true;
+        x11.enable = true;
+        size = 24;
+      };
+
+      programs.fzf = {
+        enable = true;
+      };
+
+      gtk = {
+        enable = true;
+      };
+
+      qt = {
+        enable = true;
+        platformTheme.name = "gtk";
+      };
+
+      home.stateVersion = "23.11";
+    };
   };
 }
