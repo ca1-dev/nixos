d1d9107217d4d3e4b2e22a94c6c075e7364930ad
ca1 <ca1@ca1.dev>
2026-03-14 01:49:22 +0000

    rename greetd.tuigreet to tuigreet

--- modules/nixos/gui.nix
+++ modules/nixos/gui.nix
@@ -6,7 +6,7 @@
     settings = {
       default_session = {
         command = builtins.concatStringsSep " " [
-          "${pkgs.greetd.tuigreet}/bin/tuigreet"
+          "${pkgs.tuigreet}/bin/tuigreet"
           "--cmd 'sh -c \"source /etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh && start-hyprland\"'"
           "--time"
           "--time-format '%a %d/%m %H:%M'"
